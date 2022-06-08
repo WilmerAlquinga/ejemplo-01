6b1617d351ebc5f06ff210a919ea4f3ff7c49275
Wilmer <wsalquinga@espe.edu.ec>
2022-06-08 18:54:55 -0500

    first commit

---
+++ src/main/java/ec/edu/espe/arquitectura/ejemplo/ejemplo/EjemploApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.ejemplo.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/ejemplo/ejemplo/EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.ejemplo.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
